b87f3d5e023a3d6fee65b6746dcdaf17d87afc9e
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-19 14:30:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_overriding
+{
+    internal class Shapes
+    {
+        public virtual void sides()
+        {
+            Console.WriteLine("All shapes have different sides");
+        }
+    }
+
+    internal class Circle: Shapes
+    {
+            public override void sides()
+    {
+        Console.WriteLine(" a circle has no sides!");
+    }
+
+}
+
+
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shapes shapes = new Shapes();
+            shapes.sides();
+
+            Circle circle = new Circle();
+            circle.sides();
+
+
+            Console.ReadLine();
+        }
+    }
+}
